3f9349b3a4a8d8bb9bfcd074439845c6b691bd28
jtwh-99 <74275814+jtwh-99@users.noreply.github.com>
2020-11-17 17:31:06 +0800

    Update Catalog.java

--- src/Main/Catalog.java
+++ src/Main/Catalog.java
@@ -12,6 +12,8 @@ package Main;
 public class Catalog <T extends Comparable<T>> implements CatalogInterface<T> {
      private T[] list;
   private int length;
+  private T[] detail;
+  
   private static final int limit = 20;
   
     public Catalog() {
@@ -21,22 +23,54 @@ public class Catalog <T extends Comparable<T>> implements CatalogInterface<T> {
   public Catalog(int initialCapacity) {
     length = 0;
     list = (T[]) new Comparable[initialCapacity];
+    detail=(T[]) new Comparable[initialCapacity];
+    
   }
 
-  public boolean add(T newEntry) {//save nickname 
+  public boolean add(T newEntry, T secondEntry) {//save nickname 
       int i = 0;
     while (i < length ) {
       i++;
       
     }
+    detail[i] = secondEntry;
     list[i] = newEntry;
+    
     length++;
    
     return true;
   }
-public boolean edit(T newEntry){
+ 
+public boolean edit(int cID, int choose, T newEntry){
     
-    return true;
+    if(length==0){
+       return false;
+   }
+   else{
+      int i = 0;
+    while (i < length && i < cID-1) {
+      i++;
+    }
+    if(i==cID-1){
+        
+        if(choose == 1)
+        {
+            list[i]=newEntry;
+            
+        }
+            
+        else if (choose ==2)
+        {
+            detail[i]= newEntry;
+             
+        }
+        
+    }
+    
+     return true;
+    
+   
+}
 }
 
   public boolean remove(T anEntry) {//remove the nickname
@@ -60,6 +94,68 @@ public boolean edit(T newEntry){
     length = 0;
   }
 
+  
+
+  public int getLength() {//get the array length
+    return length;
+  }
+
+  public boolean isEmpty() {//let other module may use
+    if(length==0){
+        return true;
+    }
+    else
+    return false;
+  }
+
+ public String toString() {
+    String outputStr = "";
+    for (int index = 0; index < length; ++index) {
+      outputStr += (index+1)+"."+list[index] + " ID " + (index+1)+"\n";
+    }
+    outputStr += "99. exit\n";
+outputStr +=  "Which one you want to edit?\n Please Enter ID number:";
+    return outputStr;
+  }
+ public String toString2(int cID) {
+    String outputStr = "";
+     if(length==0){
+         outputStr = "Error";
+         
+    }
+       
+   
+   else{
+      int i = 0;
+    while (i < length && i < cID-1) {
+      i++;
+    }
+    if(i==cID-1){
+    
+    outputStr+= list[i]+" ID " + (i+1) +"\n Detail: "+detail[i]+"\n";
+   
+        }
+    }
+     return outputStr;
+ }
+ 
+    
+  private boolean isArrayFull() {//for other module may use
+    return length == list.length;
+  }
+
+ 
+
+
+  private void removeGap(int givenPosition) {//remove child function.
+    int removedIndex = givenPosition - 1;
+    int lastIndex = length - 1;
+
+    for (int index = removedIndex; index < lastIndex; index++) {
+      list[index] = list[index + 1];
+    }
+  }
+}
   
 
   public int getLength() {//get the array length

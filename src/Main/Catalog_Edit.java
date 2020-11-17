49df8cf58aa8d59dd25dfc06d185d48f586345c9
jtwh-99 <74275814+jtwh-99@users.noreply.github.com>
2020-11-17 17:32:42 +0800

    Update Catalog_Edit.java

--- src/Main/Catalog_Edit.java
+++ src/Main/Catalog_Edit.java
@@ -12,16 +12,51 @@ import Main.CatalogInterface;
  * @author USER
  */
 public class Catalog_Edit {
-   CatalogInterface<String> ciList = new Catalog<String>();
+   static CatalogInterface<String> ciList = new Catalog<String>();
     
     public static void main(String[] args) {
-       
+      Scanner scan = new Scanner(System.in);
+      int control1 =  0;
+      int control2 = 0 ;
+      String editItem = "";
+      menu();
+        do 
+       {
+           System.out.print(ciList.toString());
+           try{
+               control1= Integer.parseInt(scan.nextLine());
+               if(control1==99)
+                   break;
+               System.out.println(ciList.toString2(control1));
+               System.out.println("Which one you want to edit for this?\n 1.Flower \n 2.Flower detail\n 3.Exit \n Enter :");
+               try{
+                   control2 =Integer.parseInt(scan.nextLine());
+                   if (control2 == 1 || control2 ==2)
+                   { 
+                       
+                       System.out.println("Change to ?  :");
+                   editItem = scan.nextLine();
+                   
+                   ciList.edit(control1, control2, editItem);
+                   }
+               }catch(Exception e)
+           {
+               System.out.println("Please enter number only.");
+           }
+           }catch(Exception e)
+           {
+               System.out.println("Please enter number only.");
+           }
+       }while (control1 != 99);
     }
-    public void menu(){
-        ciList.add("Orchid ID 1");
-        ciList.add("Sun Flower ID 2");
-        ciList.add("Lady Finger ID 3");
-        ciList.add("Rose ID 4");
+    public static void menu(){
+        ciList.add("Orchid","Smells good and best sell!");
+        ciList.add("Sun Flower","Easy to plant, no one can ignore it!");
+        
+        ciList.add("Lady Finger","Beautiful! you also can eat as a vage!");
+        
+        ciList.add("Rose","Opps, sold out.");
+        
         
     }
     
